e18c739c60c8a9e4c6076657be0a724b09d0b5c2
Vova <bordiian.v@gmail.com>
2023-10-19 23:36:56 +0300

    add activity detection

--- server/index.js
+++ server/index.js
@@ -25,9 +25,26 @@ const io = new Server(expressServer, {
 io.on('connection', socket => {
   console.log(`user ${socket.id} connected`)
 
+  // Upon connection - only to user
+  socket.emit('message', 'Welcome to chat App!')
+
+  // Upon connection - to all others!
+  socket.broadcast.emit('message', `user ${socket.id.substring(0, 7)} connected`)
+
+  // Listening for a message event
   socket.on('message', data => {
     console.log(data)
     io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+  })
+
+  //When user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `user ${socket.id.substring(0, 7)} disconnected`)
+  })
+
+  // Listen for Activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
   })
 })
 
